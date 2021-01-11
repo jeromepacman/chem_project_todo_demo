4dfee238cc5986d5d600f23e577b92b1820c50d9
jeromepacman <jeromepacman@gmail.com>
2021-01-11 20:24:50 +0100

    static templates

--- rarechems_core/urls.py
+++ rarechems_core/urls.py
@@ -7,7 +7,7 @@ from django.contrib.auth import views
 
 from apps.cart.webhook import webhook
 from apps.cart.views import cart_detail, success
-from apps.core.views import frontpage, contact, about
+from apps.core.views import frontpage, contact, about, terms, payment, shipping
 from apps.order.views import admin_order_pdf
 from apps.store.views import product_detail, category_detail, search
 from apps.userprofile.views import signup, myaccount
@@ -28,6 +28,9 @@ urlpatterns = [
     path('cart/success/', success, name='success'),
     path('contact/', contact, name='contact'),
     path('about/', about, name='about'),
+    path('terms/', terms, name='terms'),
+    path('payment/', payment, name='payment'),
+    path('shipping/', shipping, name='shipping'),
     path('admin/', admin.site.urls),
     path('admin/admin_order_pdf/<int:order_id>/', admin_order_pdf, name='admin_order_pdf'),
 
