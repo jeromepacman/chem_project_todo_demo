6755456a79fc146b238d4f9b6f8eab25a8e60df4
jeromepacman <jeromepacman@gmail.com>
2021-01-12 18:21:23 +0100

    favicon

--- rarechems_core/prod_settings.py
+++ rarechems_core/prod_settings.py
@@ -17,12 +17,12 @@ CSRF_COOKIE_SECURE = True
 
 DATABASES['default'] = dj_database_url.config(conn_max_age=500)
 
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'mail.infomaniak.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'contact@tradingview.com'
-EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+#'EMAIL_USE_TLS = True
+#EMAIL_HOST = 'mail.infomaniak.com'
+#EMAIL_PORT = 587
+#EMAIL_HOST_USER = 'contact@tradingview.com'
+#EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+#EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend
 
 CLOUDINARY_URL = os.environ['CLOUDINARY_URL']
 
